ccfb57357d2a994e935ef9108301885838013c85
Didi <demi.torrealba@hotmail.com>
2024-11-11 13:57:04 +0100

    entities and viewmodels update

---
+++ InvoiceManagementLibrary/Entities/InvoiceEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementLibrary.Entities;
+
+public class InvoiceEntity
+{
+    public int InvoiceId { get; set; } 
+    public DateTime Date { get; set; }
+    public int CustomerId { get; set; }
+    public int OrderId { get; set; }
+    public DateTime DueDate { get; set; }
+    public DateTime? PaidDate { get; set; }
+    public decimal Amount { get; set; }
+    public string? Status { get; set; }
+
+}
---
+++ InvoiceManagementLibrary/ViewModels/InvoiceTableViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementLibrary.ViewModels;
+
+public class InvoiceTableViewModel
+{
+  
+        public int InvoiceId { get; set; }
+        public int OrderId { get; set; }
+        public int CustomerId { get; set; }
+        public decimal Amount { get; set; }
+        public string? Status { get; set; }
+    
+}
+
+
