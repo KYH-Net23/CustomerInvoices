2d14a4884ebdee2a5e6e62de8f2d310f6ee1e4de
abd1rahmann <abdi123rahman@outlook.com>
2024-11-12 18:47:21 +0100

    Added DbContext

---
+++ InvoiceManagementLibrary/Contexts/InvoiceDbContext.cs
@@ -0,0 +1,26 @@
+﻿using InvoiceManagementLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceManagementLibrary.Contexts
+{
+    public class InvoiceDbContext : DbContext 
+    {
+     protected InvoiceDbContext()
+        {
+
+        }
+
+        public InvoiceDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<InvoiceEntity> Invoices { get; set; }
+
+    }
+}
